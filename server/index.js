289ff95a44987f953a8bd718f2d83d3866591ed9
Parshvi Dutta <parshvidutta@example.com>
2026-09-24 15:51:42 +0530

    Fix Express 5 wildcard route crash and add start script

--- server/index.js
+++ server/index.js
@@ -43,7 +43,7 @@ app.use('/api/applications', require('./routes/applications'));
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../dist')));
 
-  app.get('*', (req, res) => {
+  app.get(/.*/, (req, res) => {
     res.sendFile(path.resolve(__dirname, '../dist', 'index.html'));
   });
 }
